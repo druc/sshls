09bb83cec9d5275e6d5ad8971a5e7b9d96b86181
Druc Constantin <druc@pinsmile.com>
2018-10-17 18:07:50 +0300

    Update

--- cmds/addSshAccount.js
+++ cmds/addSshAccount.js
@@ -2,33 +2,40 @@
 const inquirer = require("inquirer");
 const Fs = require('fs');
 const chalk = require('chalk');
+// const STORAGE_PATH = require('os').homedir() + '/sshls.json';
+const STORAGE_PATH = 'sshls.json';
 
 module.exports = () => {
-    let SSHLS = [];
-    // const STORAGE_PATH = require('os').homedir() + '/sshls.json';
-    const STORAGE_PATH = 'sshls.json';
-    // Check if file exists, if not, create it.
-    try {
-        Fs.statSync(STORAGE_PATH);
-    } catch (e) {
-        Fs.writeFile(STORAGE_PATH, JSON.stringify([], null, 2), function() {});
-    }
-
-    Fs.readFile(STORAGE_PATH, "utf-8", function (err, content) {
-        if (err) {
-            console.log(err);
-        }
+    
+    const createFileIfMissing = () => {
         try {
-            content = JSON.parse(content);
+            Fs.statSync(STORAGE_PATH);
         } catch (e) {
-            err = e;
+            Fs.writeFile(STORAGE_PATH, JSON.stringify([], null, 2), function () {
+            });
+        }
+    };
+
+    const fetchRecords = (callback) => {
+        if (!callback) {
+            console.log(chalk.red('Something wrong happened.'));
+            return;
         }
 
-        SSHLS = content;
-        
-        console.log(SSHLS);
-    });
+        Fs.readFile(STORAGE_PATH, "utf-8", function (err, content) {
+            if (err) {
+                console.log(chalk.red('No ssh records.'));
+                return;
+            }
+            try {
+                content = JSON.parse(content);
+            } catch (e) {
+                console.log(chalk.red('Failed to parse ssh records.'));
+            }
 
+            return callback(content);
+        });
+    };
 
     const askQuestions = () => {
         const questions = [
@@ -53,12 +60,30 @@ module.exports = () => {
                 message: "Enter password:"
             }
         ];
-        
+
         return inquirer.prompt(questions);
     };
-    
+
     const run = async () => {
-        const answers = await askQuestions();
+        createFileIfMissing();
+        let answers = await askQuestions();
+
+        const updateRecords = (data) => {
+            data.push({
+                id: data[data.length - 1].id + 1,
+                name: answers.name,
+                connect: answers.user + '@' + answers.host,
+                password: answers.password,
+                user: answers.user,
+                host: answers.host,
+            });
+
+            Fs.writeFile(STORAGE_PATH, JSON.stringify(data, null, 2), function () {
+                console.log(chalk.green('Record stored.'));
+            });
+        };
+
+        fetchRecords(updateRecords);
     };
 
     run();
--- cmds/listSshAccounts.js
+++ cmds/listSshAccounts.js
@@ -1,46 +1,50 @@
 const t = require('table');
 const Fs = require('fs');
+const chalk = require('chalk');
+
+const STORAGE_PATH = 'sshls.json';
 
 module.exports = () => {
-    const STORAGE_PATH = 'sshls.json';
 
-    const run = async () => {
-        let data,
-            output;
-
-        data = [
+    const displayRecords = (content) => {
+        let data = [
             ['#', 'Name', 'Connect', 'Password', 'User', 'Host'],
-            ['1', 'Test', 'web@test', 'test', 'web', 'test'],
         ];
 
+        content.forEach((item) => {
+            data.push([
+                item.id,
+                item.name,
+                item.connect,
+                item.password,
+                item.user,
+                item.host
+            ]);
+        });
+
+        console.log(t.table(data, {}));
+    };
+
+    const fetchRecords = (callback) => {
+        if (!callback) {
+            console.log(chalk.red('Something wrong happened.'));
+            return;
+        }
+
         Fs.readFile(STORAGE_PATH, "utf-8", function (err, content) {
-            if (err) {
-                console.log(err);
+            if (err || !content.length) {
+                console.log(chalk.red('No ssh records.'));
+                return;
             }
             try {
                 content = JSON.parse(content);
             } catch (e) {
-                console.log(e);
+                console.log(chalk.red('Failed to parse ssh records.'));
             }
 
-            data = [];
-
-            content.forEach((item) => {
-                data.push([
-                    item.id,
-                    item.name,
-                    item.name,
-                    item.command,
-                    item.host,
-                    item.password
-                ]);
-            });
-            
-            output = t.table(data, {});
-
-            console.log(output);
+            return callback(content);
         });
     };
 
-    run();
+    fetchRecords(displayRecords);
 };
